ae03c0e788c6e3dcf5b803b59c49e890aad0f778
mbadalamente <70178700+mbadalamente@users.noreply.github.com>
2020-10-22 19:37:04 -0400

    Update GamePanel.java

--- SER-225-Game-master/src/Engine/GamePanel.java
+++ SER-225-Game-master/src/Engine/GamePanel.java
@@ -1,10 +1,15 @@
 package Engine;
 
 import GameObject.Rectangle;
+import Screens.PlayLevelScreen;
 import SpriteFont.SpriteFont;
 import Utils.Colors;
 
 import javax.swing.*;
+
+import Game.GameState;
+import Game.ScreenCoordinator;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,8 +20,12 @@ import java.awt.event.ActionListener;
  */
 public class GamePanel extends JPanel {
 	// loads Screens on to the JPanel
-	// each screen has its own update and draw methods defined to handle a "section" of the game.
+	// each screen has its own update and draw methods defined to handle a "section"
+	// of the game.
 	private ScreenManager screenManager;
+	
+	//****
+	private ScreenCoordinator screenCoordinator;
 
 	// used to create the game loop and cycle between update and draw calls
 	private Timer timer;
@@ -43,14 +52,17 @@ public class GamePanel extends JPanel {
 		graphicsHandler = new GraphicsHandler();
 
 		screenManager = new ScreenManager();
-		
+
 		pauseLabel = new SpriteFont("PAUSE", 365, 280, "Comic Sans", 24, Color.white);
 		pauseLabel.setOutlineColor(Color.black);
 		pauseLabel.setOutlineThickness(2.0f);
 
-		// Every timer "tick" will call the update method as well as tell the JPanel to repaint
-		// Remember that repaint "schedules" a paint rather than carries it out immediately
-		// If the game is really laggy/slow, I would consider upping the FPS in the Config file.
+		// Every timer "tick" will call the update method as well as tell the JPanel to
+		// repaint
+		// Remember that repaint "schedules" a paint rather than carries it out
+		// immediately
+		// If the game is really laggy/slow, I would consider upping the FPS in the
+		// Config file.
 		timer = new Timer(1000 / Config.FPS, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				update();
@@ -59,9 +71,15 @@ public class GamePanel extends JPanel {
 		});
 		timer.setRepeats(true);
 	}
+	
+	// sets the screencoordinator to the one being used
+	public void setScreenCoordinator(ScreenCoordinator sc) {
+		this.screenCoordinator = sc;
+	}
 
 	// this is called later after instantiation, and will initialize screenManager
-	// this had to be done outside of the constructor because it needed to know the JPanel's width and height, which aren't available in the constructor
+	// this had to be done outside of the constructor because it needed to know the
+	// JPanel's width and height, which aren't available in the constructor
 	public void setupGame() {
 		setBackground(Colors.CORNFLOWER_BLUE);
 		screenManager.initialize(new Rectangle(getX(), getY(), getWidth(), getHeight()));
@@ -77,10 +95,13 @@ public class GamePanel extends JPanel {
 		return screenManager;
 	}
 
+	// only lets the game be paused if the game state is the level 
 	public void update() {
-		if (Keyboard.isKeyDown(pauseKey) && !keyLocker.isKeyLocked(pauseKey)) {
-			isGamePaused = !isGamePaused;
-			keyLocker.lockKey(pauseKey);
+		if(screenCoordinator.getGameState() == GameState.LEVEL) {
+			if (Keyboard.isKeyDown(pauseKey) && !keyLocker.isKeyLocked(pauseKey)) {
+				isGamePaused = !isGamePaused;
+				keyLocker.lockKey(pauseKey);
+			}
 		}
 		
 		if (Keyboard.isKeyUp(pauseKey)) {
@@ -98,7 +119,8 @@ public class GamePanel extends JPanel {
 		// if game is paused, draw pause gfx over Screen gfx
 		if (isGamePaused) {
 			pauseLabel.draw(graphicsHandler);
-			graphicsHandler.drawFilledRectangle(0, 0, ScreenManager.getScreenWidth(), ScreenManager.getScreenHeight(), new Color(0, 0, 0, 100));
+			graphicsHandler.drawFilledRectangle(0, 0, ScreenManager.getScreenWidth(), ScreenManager.getScreenHeight(),
+					new Color(0, 0, 0, 100));
 		}
 	}
 
@@ -106,7 +128,8 @@ public class GamePanel extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		// every repaint call will schedule this method to be called
-		// when called, it will setup the graphics handler and then call this class's draw method
+		// when called, it will setup the graphics handler and then call this class's
+		// draw method
 		graphicsHandler.setGraphics((Graphics2D) g);
 		if (doPaint) {
 			draw();
